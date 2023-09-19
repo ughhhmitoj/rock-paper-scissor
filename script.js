40afaa622431574c1dce16c3686e23497392c9ea
ughhhmitoj <77149099+ughhhmitoj@users.noreply.github.com>
2023-09-19 22:44:43 +0530

    Write code for function playRound and Log the game play in console

--- script.js
+++ script.js
@@ -9,22 +9,57 @@ function getComputerChoice(){
     computerChoice = choices[index];
     return computerChoice;
 }
-console.log(getComputerChoice());
+
+
 
 function playRound(playerChoice,computerChoice){
+
     /*if player choice is paper 
             and if computer choice is scissor, then lose
             and if computer choice is rock, then win
             else tie*/ 
-    
+    if (playerChoice == "paper"){
+        if (computerChoice== "Scissors"){
+            return ("You Lose!")
+        } else if 
+        (computerChoice=="Rock"){
+            return ("You Win!")
+        } else
+        return ("Tie!")
+    };    
     /*if player choice is scissor
             and if computer choice is rock, then lose
             and if computer choice is paper, then win
             else tie*/ 
+    if (playerChoice == "scissors"){
+        if (computerChoice== "Rock"){
+            return ("You Lose!")
+        } else if 
+        (computerChoice=="Paper"){
+            return ("You Win!")
+        } else
+        return ("Tie!")
+    };    
+     
     
     /*if player choice is rock
             and if computer choice is paper, then lose
             and if computer choice is scissor, then win
             else tie*/ 
     
+    if (playerChoice == "rock"){
+        if (computerChoice== "Paper"){
+            return ("You Lose!")
+        } else if 
+        (computerChoice=="Scissors"){
+            return ("You Win!")
+        } else
+        return ("Tie!")
+    };    
+    
 };
+const playerChoice = prompt("What?").toLowerCase();
+console.log(playerChoice);
+const computerChoice = getComputerChoice();
+console.log(computerChoice);
+console.log(playRound(playerChoice, computerChoice));
