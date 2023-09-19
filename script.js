c8f89aaa91653cb0bd9675411b6dea528e33f139
ughhhmitoj <77149099+ughhhmitoj@users.noreply.github.com>
2023-09-19 20:30:24 +0530

    Write code for function getComputerChoice

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function getComputerChoice(){
     /*Make an array of avaliable choices */
+    let choices = ["Rock", "Paper", "Scissors"];
     /*make a variable called computerChoice */
-    /* Randomly select and return one of the three options of array*/
+    let computerChoice;
+    /* Randomly select a number responding to one of the three options of array*/
+    let index = Math.floor(Math.random()*choices.length);
+    /*Choose an option corresponding to the number returned in index */
+    computerChoice = choices[index];
+    return computerChoice;
 }
