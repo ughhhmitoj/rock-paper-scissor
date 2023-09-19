dfe451d138ff8440c93e0004c584806115099acf
ughhhmitoj <77149099+ughhhmitoj@users.noreply.github.com>
2023-09-19 20:17:05 +0530

    Write pseudocode for getComputerChoice

---
+++ script.js
@@ -0,0 +1,5 @@
+function getComputerChoice(){
+    /*Make an array of avaliable choices */
+    /*make a variable called computerChoice */
+    /* Randomly select and return one of the three options of array*/
+}
